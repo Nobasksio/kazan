376a787b4f2986a09819a17929e708cfc55182bd
tginweb <46878183+tginweb@users.noreply.github.com>
2019-09-29 10:23:22 +0800

    test

--- api/src/Controller/MainController.php
+++ api/src/Controller/MainController.php
@@ -3,129 +3,188 @@
 namespace App\Controller;
 
 use App\Entity\Rest;
+use App\Entity\TrainRoute;
 use App\Repository\KitchenTypeRepository;
 use App\Repository\ProductCatRepository;
 use App\Repository\ProductRepository;
 use App\Repository\RestRepository;
 use App\Repository\StationRepository;
+use App\Repository\TrainRepository;
+use App\Repository\TrainRouteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MainController extends AbstractController
-{
-    /**
-     * @Route("/api/dictionary", name="dict", methods={GET})
-     */
-    public function index(KitchenTypeRepository $kitchenTypeRepository,
-                          ProductCatRepository $productCatRepository)
-    {
+class MainController extends AbstractController {
 
-        $kitchen_types = $kitchenTypeRepository->findAll();
+	public function index(
+		KitchenTypeRepository $kitchenTypeRepository,
+		ProductCatRepository $productCatRepository
+	) {
 
-        $kitchen_types_arr = [];
+		$kitchen_types = $kitchenTypeRepository->findAll();
 
-        foreach ($kitchen_types as $item){
-            $kitchen_types_arr[] = ['id'=>$item->getId(),
-                'name'=>$item->getName(),
-                ];
-        }
+		$kitchen_types_arr = [];
 
-        $p_cats = $productCatRepository->findAll();
+		foreach ( $kitchen_types as $item ) {
+			$kitchen_types_arr[] = [
+				'id'   => $item->getId(),
+				'name' => $item->getName(),
+			];
+		}
 
-
-        $p_cats_arr = [];
-
-        foreach ($p_cats as $item){
-            $p_cats_arr[] = ['id'=>$item->getId(),
-                'name'=>$item->getName(),
-            ];
-        }
-
-        $response = json_encode(['product_cat'=>$p_cats_arr,
-            'kitchen_type' =>$kitchen_types_arr
-            ]);
+		$p_cats = $productCatRepository->findAll();
 
 
-        return JsonResponse::fromJsonString($response);
-    }
+		$p_cats_arr = [];
 
-    /**
-     * @Route("/api/rest/list", name="rest_station", methods={GET})
-     */
-    public function rest_station(RestRepository $restRepository, Request $request, StationRepository $stationRepository)
-    {
+		foreach ( $p_cats as $item ) {
+			$p_cats_arr[] = [
+				'id'   => $item->getId(),
+				'name' => $item->getName(),
+			];
+		}
 
-
-        $station_id = $request->request->get('station_id');
-
-        $rests = $restRepository->findAll();
-
-        $len = count($rests);
-
-        $rest_arr = [];
-
-        for($i=0;$i<5;$i++){
-            $key = rand(0,$len-1);
-
-            $rest_arr[] = ['id' => $rests['$key']->getId(),
-                'name' => $rests['$key']->getName(),
-                'description'=> $rests['$key']->getDescription(),
-                ];
-        }
-
-        $response = json_encode(['rests'=>$rest_arr
-        ]);
+		$response = json_encode( [
+			'product_cat'  => $p_cats_arr,
+			'kitchen_type' => $kitchen_types_arr
+		] );
 
 
+		return JsonResponse::fromJsonString( $response );
+	}
+
+	/**
+	 * ("/api/rest/list", name="rest_station", methods={GET})
+	 */
+	public function rest_list( RestRepository $restRepository, Request $request, StationRepository $stationRepository ) {
 
 
-        return JsonResponse::fromJsonString($response);
-    }
+		$station_id = $request->request->get( 'station_id' );
 
-    /**
-     * @Route("/api/rest/{id}/load", name="rest_station", methods={GET})
-     */
-    public function rest(Rest $rest, ProductRepository $productRepository)
-    {
+		$rests = $restRepository->findAll();
 
-        $products = $productRepository->findAll();
+		$len = count( $rests );
 
-        $len = count($products);
-        $len_resp = rand(3,6);
-        for($i=0;$i<$len_resp;$i++){
-            $key = rand(0,$len-1);
+		$rest_arr = [];
 
-            $products_arr[] = ['id' => $products['$key']->getId(),
-                'name' => $products['$key']->getName(),
-                'description'=> $products['$key']->getDescription(),
-                'price'=> $products['$key']->getPrice(),
-                'kitchen_type'=> ['id'=>$products['$key']->getKitchenType()->getId(),
-                    'name'=>$products['$key']->getKitchenType()->getName(),
-                    ],
-                'product_cat'=> ['id'=>$products['$key']->getProductCat()->getId(),
-                    'id'=>$products['$key']->getProductCat()->getName(),
-                    ],
-                'ves'=> $products['$key']->getVes(),
-            ];
-        }
+		for ( $i = 0; $i < 5; $i ++ ) {
+			$key = rand( 0, $len - 1 );
 
-        $rest_arr= ['id' => $rest->getId(),
-            'name' => $rest->getName(),
-            'description'=> $rest->getDescription(),
-        ];
+			$rest_arr[] = [
+				'id'          => $rests['$key']->getId(),
+				'name'        => $rests['$key']->getName(),
+				'description' => $rests['$key']->getDescription(),
+			];
+		}
 
-        $response = json_encode(['rest'=>$rest_arr,
-            'products'=>$products_arr
-        ]);
-
-        return JsonResponse::fromJsonString($response);
-
-    }
+		$response = json_encode( [
+			'rests' => $rest_arr
+		] );
 
 
+		return JsonResponse::fromJsonString( $response );
+	}
+
+	/**
+	 * ("/api/rest/{id}/load", name="rest_station", methods={GET})
+	 */
+	public function rest_load( Rest $rest, ProductRepository $productRepository ) {
+
+		$products = $productRepository->findAll();
+
+		$len      = count( $products );
+		$len_resp = rand( 3, 6 );
+		for ( $i = 0; $i < $len_resp; $i ++ ) {
+			$key = rand( 0, $len - 1 );
+
+			$products_arr[] = [
+				'id'           => $products['$key']->getId(),
+				'name'         => $products['$key']->getName(),
+				'description'  => $products['$key']->getDescription(),
+				'price'        => $products['$key']->getPrice(),
+				'kitchen_type' => [
+					'id'   => $products['$key']->getKitchenType()->getId(),
+					'name' => $products['$key']->getKitchenType()->getName(),
+				],
+				'product_cat'  => [
+					'id' => $products['$key']->getProductCat()->getId(),
+					'id' => $products['$key']->getProductCat()->getName(),
+				],
+				'ves'          => $products['$key']->getVes(),
+			];
+		}
+
+		$rest_arr = [
+			'id'          => $rest->getId(),
+			'name'        => $rest->getName(),
+			'description' => $rest->getDescription(),
+		];
+
+		$response = json_encode( [
+			'rest'     => $rest_arr,
+			'products' => $products_arr
+		] );
+
+		return JsonResponse::fromJsonString( $response );
+
+	}
 
 
+	/**
+	 * ("/api/train/list", name="rest_station", methods={GET})
+	 */
+	public function train_list( TrainRepository $trainRepository ) {
+
+		$trains = $trainRepository->findAll();
+
+		$train_result = [];
+
+		foreach ( $trains as $item ) {
+			$train_result[] = [
+				'id'   => $item->getId(),
+				'name' => $item->getName(),
+			];
+		}
+
+		$response = json_encode( [
+			'trains'  => $train_result,
+		] );
+
+		return JsonResponse::fromJsonString( $response );
+
+	}
+
+
+	/**
+	 * ("/api/train/list", name="rest_station", methods={GET})
+	 */
+	public function train_route( Request $request, TrainRouteRepository $trainRouteRepository ) {
+
+		$train_id = $request->request->get( 'train_id' );
+
+		$routes = $trainRouteRepository->findBy(array('train' => $_GET['train_id']));
+
+		$routes_result = [];
+
+		foreach ( $routes as $item ) {
+			$routes_result[] = [
+				'id'   => $item->getId(),
+				'train_id' => $item->getTrain()->getId(),
+				'station_id' => $item->getStation()->getId(),
+				'station_name' => $item->getStation()->getName(),
+				'arrival_time' => $item->getArrivalTime(),
+				'departure_time' => $item->getDepartureTime(),
+			];
+		}
+
+		$response = json_encode( [
+			'routes'  => $routes_result,
+		] );
+
+		return JsonResponse::fromJsonString( $response );
+
+	}
 }
