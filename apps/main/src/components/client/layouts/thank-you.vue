db455f1fc95808b72801dedee83f6e68d159453f
Артур <a420673@yandex.ru>
2019-09-29 08:21:24 +0300

    fix

---
+++ apps/main/src/components/client/layouts/thank-you.vue
@@ -0,0 +1,24 @@
+<template>
+
+    <div>
+
+
+
+    </div>
+
+</template>
+
+<script>
+
+import AppCommon from './../common/common'
+
+export default {
+  components: {
+      AppCommon
+  },
+  data: () => ({
+      drawer: true
+  }),
+};
+
+</script>
